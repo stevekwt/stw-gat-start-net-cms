31622f0c18c079eb3dc27c87b899ad90ac9e870b
Stephen Thomas <66618292+stevekwt@users.noreply.github.com>
2022-04-21 22:15:01 -0700

    register previews for TJ and MM pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,8 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import AboutPagePreview from './preview-templates/TheJokesPagePreview'
+import AboutPagePreview from './preview-templates/MissMiseryPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
@@ -12,5 +14,7 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('about', TheJokesPagePreview)
+CMS.registerPreviewTemplate('about', MissMiseryPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
